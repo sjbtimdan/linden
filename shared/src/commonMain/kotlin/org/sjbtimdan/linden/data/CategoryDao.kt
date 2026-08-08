4a9d5e91251443e084443d2d2e4631da4c13a4e9
stetimi <stetimi@users.noreply.github.com>
2026-08-08 11:46:09 +0800

    refactor: reduce CategoryDao.create to a single insert query

--- shared/src/commonMain/kotlin/org/sjbtimdan/linden/data/CategoryDao.kt
+++ shared/src/commonMain/kotlin/org/sjbtimdan/linden/data/CategoryDao.kt
@@ -1,19 +1,14 @@
 package org.sjbtimdan.linden.data
 
 import app.cash.sqldelight.async.coroutines.awaitAsList
-import app.cash.sqldelight.async.coroutines.awaitAsOne
 import org.sjbtimdan.linden.CategoryEntity
 import org.sjbtimdan.linden.CategoryQueries
 import org.sjbtimdan.linden.model.Category
 import org.sjbtimdan.linden.model.CategoryType
 
 class CategoryDao(private val queries: CategoryQueries) {
-    suspend fun create(name: String, type: CategoryType): Category {
-        return queries.transactionWithResult {
-            queries.insert(name, type.name)
-            val id = queries.lastInsertId().awaitAsOne()
-            queries.selectById(id).awaitAsOne().toCategory()
-        }
+    suspend fun create(name: String, type: CategoryType) {
+        queries.insert(name, type.name)
     }
 
     suspend fun update(category: Category): Unit {
--- shared/src/commonTest/kotlin/org/sjbtimdan/linden/data/CategoryDaoTest.kt
+++ shared/src/commonTest/kotlin/org/sjbtimdan/linden/data/CategoryDaoTest.kt
@@ -2,18 +2,23 @@ package org.sjbtimdan.linden.data
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.sjbtimdan.linden.model.Category
 import org.sjbtimdan.linden.model.CategoryType
 
 class CategoryDaoTest : StringSpec({
     "CRUD operations should work" {
         val database = lindenDatabase()
         val dao = CategoryDao(database.categoryQueries)
+
         dao.getAll() shouldBe emptyList()
-
-        val created = dao.create("Groceries", CategoryType.Expense)
-        dao.getAll() shouldBe listOf(created)
-
-        val updated = created.copy(name = "Bonus", type = CategoryType.Both)
+        dao.create("Groceries", CategoryType.Expense)
+        val allCreated = dao.getAll()
+        allCreated shouldBe listOf(Category(
+            id = allCreated.first().id,
+            name = "Groceries",
+            type = CategoryType.Expense
+        ))
+        val updated = allCreated.first().copy(name = "Bonus", type = CategoryType.Both)
         dao.update(updated)
         dao.getAll() shouldBe listOf(updated)
     }
