8975b2ef2b702665cff1ffd3bec0230773cce3e9
stetimi <stetimi@users.noreply.github.com>
2026-09-15 19:14:38 +0800

    chore: versions plugin upgrade

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.github.ben-manes.versions.settings") version "0.61.0"
+    id("io.github.ben-manes.versions.settings") version "0.63.0"
 }
 
 dependencyResolutionManagement {
